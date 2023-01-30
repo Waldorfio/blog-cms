dd1dbb04d88adbd339c0f74a25f8d7d46667e299
Walid VM <walid.hanifi707@gmail.com>
2023-01-30 15:42:18 +1000

    Initial commit

---
+++ src/RouteSwitch.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Home from './Home';
+
+function RouteSwitch() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          {/* <Route path="register" element={<Register />} />
+          <Route path="posts/:id" element={<Post />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default RouteSwitch;
