75dc5f6bbb3e3f66c806d5dabccee67bc1205e9f
Wally <walid.hanifi707@gmail.com>
2023-02-01 14:42:24 +1000

    Add boilerplate form data

--- src/Users.js
+++ src/Users.js
@@ -4,7 +4,20 @@ import { useOutletContext, Link } from 'react-router-dom';
 
 function Users() {
   return (
-    <main>
+    <main id="home">
+      <div id="welcome">Create, Read, Update and Delete the Users below.</div>
+      <div className="form-content">
+
+        <form id="users-form" className="cms-form" action="" method="post">
+            <input type="text" name="username" value="Waldorfio" disabled>
+            <input type="text" name="email" value="test@gmail.com" disabled>
+            <input type="password" name="password" value="password" disabled>
+            <input type="button" value="EDIT">
+            <input type="submit" value="DELETE">
+            <input type="button" value="UPDATE" disabled>
+        </form>
+
+      </div>
     </main>
   );
 }
