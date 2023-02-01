10d52ed249a1e614aa52243959dab85e6f9bba93
Wally <walid.hanifi707@gmail.com>
2023-02-01 14:41:53 +1000

    Add boilerplate form data

--- src/Messages.js
+++ src/Messages.js
@@ -4,7 +4,27 @@ import { useOutletContext, Link } from 'react-router-dom';
 
 function Messages() {
   return (
-    <main>
+    <main id="home">
+      <div id="welcome">Create, Read, Update and Delete the Messages below.</div>
+      <h4 className="form-content" id="msg-content">
+        First Post
+        <form className="cms-form" action="" method="post">
+            <input type="text" name="username" value="Waldorfio" disabled>
+            <input type="date" name="date" value="1/02/2023" disabled>
+            <input type="textarea" name="message" value="This is a test message.">
+            <input type="button" value="EDIT">
+            <input type="submit" value="DELETE">
+            <input type="button" value="UPDATE" disabled>
+        </form>
+        <form className="cms-form" action="" method="post">
+            <input type="text" name="username" value="Waldorfio" disabled>
+            <input type="date" name="date" value="1/02/2023" disabled>
+            <input type="textarea" name="message" value="This is a test message.">
+            <input type="button" value="EDIT">
+            <input type="submit" value="DELETE">
+            <input type="button" value="UPDATE" disabled>
+        </form>
+      </h4>
     </main>
   );
 }
