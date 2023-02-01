210cc0e99b3293166d4481bb70123eedf6acb4d5
Wally <walid.hanifi707@gmail.com>
2023-02-01 14:45:37 +1000

    Add user, post, and message routes for crud ops

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Home from './Home';
+import Users from './Users';
+import Posts from './Posts';
+import Messages from './Messages';
 
 function RouteSwitch() {
   return (
@@ -9,8 +12,10 @@ function RouteSwitch() {
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
-          {/* <Route path="register" element={<Register />} />
-          <Route path="posts/:id" element={<Post />} /> */}
+          <Route path="register" element={<Register />} />
+          <Route path="users" element={<Users />} />
+          <Route path="posts" element={<Posts />} />
+          <Route path="messages" element={<Messages />} />
         </Route>
       </Routes>
     </BrowserRouter>
