060ba3ca3f475b1339b62798c39f8542d6f57eb8
Wally <walid.hanifi707@gmail.com>
2023-02-01 14:29:10 +1000

    Delete unused code

--- src/App.js
+++ src/App.js
@@ -3,41 +3,6 @@ import { Outlet } from 'react-router-dom';
 import './styles/style.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
-function App() {
-  return (
-    <div>
-      <Header />
-
-      <Outlet />
-
-      <Footer />
-    </div>
-  );
-}
-
-export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import React, { useState, useEffect, useRef } from 'react';
-import { Outlet } from 'react-router-dom';
-import './styles/style.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import Success from './components/Success';
 
 function App() {
