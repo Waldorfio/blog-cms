6bae20c231cdaeaf57658361c06e71d1da8c52ef
Wally <walid.hanifi707@gmail.com>
2023-02-01 14:39:01 +1000

    Initial commit

---
+++ src/Posts.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-underscore-dangle */
+import React, { useState, useEffect } from 'react';
+import { useOutletContext, Link } from 'react-router-dom';
+
+function Posts() {
+  return (
+    <main>
+    </main>
+  );
+}
+
+export default Posts;
