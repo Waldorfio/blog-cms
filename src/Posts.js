e6fa35766bae40667834c0dea351debf77386ced
Wally <walid.hanifi707@gmail.com>
2023-02-01 14:41:12 +1000

    Add boilerplate form data

--- src/Posts.js
+++ src/Posts.js
@@ -4,7 +4,22 @@ import { useOutletContext, Link } from 'react-router-dom';
 
 function Posts() {
   return (
-    <main>
+    <main id="home">
+      <div id="welcome">Create, Read, Update and Delete the Posts below.</div>
+      <div className="form-content">
+
+        <form id="posts-form" className="cms-form" action="" method="post">
+            <input type="text" name="title" value="Test Title" disabled>
+            <input type="text" name="username" value="Waldorfio" disabled>
+            <input type="date" name="date" value="1/02/2023" disabled>
+            <input type="textarea" name="content" value="Lorem ipsum dored">
+            <!-- <textarea id="mytextarea" name="content" value="Lorum ipsum dored"></textarea> -->
+            <input type="button" value="EDIT">
+            <input type="submit" value="DELETE">
+            <input type="button" value="UPDATE" disabled>
+        </form>
+
+      </div>
     </main>
   );
 }
