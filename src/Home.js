d9eeb6b608a40dc6c4010dca73726e3e2bbf5455
Wally <walid.hanifi707@gmail.com>
2023-02-01 14:32:50 +1000

    Add boilerplate homepage menu

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,35 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Outlet } from 'react-router-dom';
-import './styles/style.css';
+/* eslint-disable no-underscore-dangle */
+import React, { useState, useEffect } from 'react';
+import { useOutletContext, Link } from 'react-router-dom';
 
 function Home() {
+  const [user, isLoggedIn] = useOutletContext();
+
   return (
-    <div id='home'>
-    </div>
+    <main id="home">
+      { isLoggedIn ? (
+        // eslint-disable-next-line react/jsx-one-expression-per-line
+        <div id="welcome">Welcome to the Content Management System {user.username}.<br>Please click on one of the links below to get started.</div>
+      ) : (
+        <div id="welcome">Please log in as an administrator to gain access to the Content Management System.</div>
+      )}
+    
+      <div id="content">
+
+        <div id='user-card' className="post">
+          <a href="/users"><h2>Users</h2></a>
+        </div>
+
+        <div id='post-card'className="post">
+          <a href="/posts"><h2>Posts</h2></a>
+        </div>
+
+        <div id='msg-card' className="post">
+          <a href="/messages"><h2>Messages</h2></a>
+        </div>
+
+      </div>
+    </main>
   );
 }
 
