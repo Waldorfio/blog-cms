905945408cb030e01468b9fd5743837e34573c6a
Walid VM <walid.hanifi707@gmail.com>
2023-02-03 13:55:15 +1000

    Add create user functionality

--- src/Users.js
+++ src/Users.js
@@ -5,17 +5,39 @@ import { useOutletContext, Link } from 'react-router-dom';
 function Users() {
   const [user, isLoggedIn, users, posts, msgs] = useOutletContext();
 
-  // DELETE API Call
+  // --------- CREATE User ---------
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+    admin: false,
+  });
+
+  const addUser = async (e) => {
+    e.preventDefault();
+    try {
+    // Send post request
+      const res = await fetch('https://blog-api-production-6aeb.up.railway.app/users/create', {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json; charset=UTF-8' },
+        body: JSON.stringify(formData),
+      });
+      // Check data
+      const data = await res.json();
+      if (data.success) {
+        console.log('Registration successful');
+      } else { console.log('data was not a success'); }
+    } catch (err) { console.error(err); }
+  };
+
+  // --------- UPDATE User ---------
+
+  // --------- DELETE User ---------
   const delUser = async (usr) => {
     try {
       await fetch(`https://blog-api-production-6aeb.up.railway.app/users/${usr._id}/delete`, { method: 'DELETE' });
       console.log(`User deleted! ${usr._id}`);
     } catch (err) { console.error(err); }
   };
-  // // Refresh API Call
-  // useEffect(() => {
-  //   delUser();
-  // }, []);
 
   return (
     <main id="home">
@@ -27,15 +49,39 @@ function Users() {
           <span>Admin</span>
         </div>
         { users.map((usr) => (
-          <form id="users-form" className="cms-form">
-            <input type="text" name="username" value={usr.username} disabled />
-            <input type="text" name="password" value={usr.password} disabled />
-            <input type="checkbox" name="admin" value={usr.admin} disabled />
+          <form className="cms-form">
+            <input type="text" name="username" value={usr.username} />
+            <input type="text" name="password" value={usr.password} />
+            <input type="checkbox" name="admin" value={usr.admin} />
             <input type="button" value="EDIT" />
             <input type="button" value="DELETE" onClick={() => delUser(usr)} />
-            <input type="button" value="UPDATE" disabled />
+            <input type="button" value="UPDATE" />
           </form>
         )) }
+
+        <form id="cms-newuser" className="cms-form">
+          <input
+            type="text"
+            name="username"
+            placeholder="Enter username"
+            value={formData.username}
+            onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+          />
+          <input
+            type="text"
+            name="password"
+            placeholder="Enter password"
+            value={formData.password}
+            onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+          />
+          <input
+            type="checkbox"
+            name="admin"
+            value={formData.admin}
+            onChange={(e) => setFormData({ ...formData, admin: e.target.value })}
+          />
+          <input type="button" value="CREATE" onClick={addUser} />
+        </form>
 
       </div>
     </main>
